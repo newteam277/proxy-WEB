e861b958a3c9567eb92c3e7d0b6aacac0e16c2f2
yciv587 <yciv587@gmail.com>
2024-01-03 01:24:41 +0800

    update：格式化代码，关闭方法未使用和属性未使用报错提示

--- src/views/router/GoRouter.api.ts
+++ src/views/router/GoRouter.api.ts
@@ -1,17 +1,18 @@
-import {defHttp} from '/@/utils/http/axios';
-import { useMessage } from "/@/hooks/web/useMessage";
+import { defHttp } from '/@/utils/http/axios';
+import { useMessage } from '/@/hooks/web/useMessage';
 
 const { createConfirm } = useMessage();
 
 enum Api {
   list = '/router/goRouter/list',
-  save='/router/goRouter/add',
-  edit='/router/goRouter/edit',
+  save = '/router/goRouter/add',
+  edit = '/router/goRouter/edit',
   deleteOne = '/router/goRouter/delete',
   deleteBatch = '/router/goRouter/deleteBatch',
   importExcel = '/router/goRouter/importExcel',
   exportXls = '/router/goRouter/exportXls',
 }
+
 /**
  * 导出api
  * @param params
@@ -25,17 +26,16 @@ export const getImportUrl = Api.importExcel;
  * 列表接口
  * @param params
  */
-export const list = (params) =>
-  defHttp.get({url: Api.list, params});
+export const list = (params) => defHttp.get({ url: Api.list, params });
 
 /**
  * 删除单个
  */
-export const deleteOne = (params,handleSuccess) => {
-  return defHttp.delete({url: Api.deleteOne, params}, {joinParamsToUrl: true}).then(() => {
+export const deleteOne = (params, handleSuccess) => {
+  return defHttp.delete({ url: Api.deleteOne, params }, { joinParamsToUrl: true }).then(() => {
     handleSuccess();
   });
-}
+};
 /**
  * 批量删除
  * @param params
@@ -48,17 +48,25 @@ export const batchDelete = (params, handleSuccess) => {
     okText: '确认',
     cancelText: '取消',
     onOk: () => {
-      return defHttp.delete({url: Api.deleteBatch, data: params}, {joinParamsToUrl: true}).then(() => {
-        handleSuccess();
-      });
-    }
+      return defHttp
+        .delete(
+          {
+            url: Api.deleteBatch,
+            data: params,
+          },
+          { joinParamsToUrl: true }
+        )
+        .then(() => {
+          handleSuccess();
+        });
+    },
   });
-}
+};
 /**
  * 保存或者更新
  * @param params
  */
 export const saveOrUpdate = (params, isUpdate) => {
-  let url = isUpdate ? Api.edit : Api.save;
-  return defHttp.post({url: url, params});
-}
+  const url = isUpdate ? Api.edit : Api.save;
+  return defHttp.post({ url: url, params });
+};
